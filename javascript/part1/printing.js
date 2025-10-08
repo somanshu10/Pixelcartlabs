9aa6ccda85360e466ec80710a792e7d8a93a0322
Kritika Sethi <kritikasethi@192.168.1.17>
2025-10-08 12:15:53 +0530

    change36

--- javascript/part1/printing.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello, World!");   
-console.table([{ a: 1, b: 2 }, { a: 3, b: 4 }]);
--- jsFoundation/part1/printing.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello");
-console.log("chai");
-process.stdout.write("chai");
-process.stdout.write("chai");
-
-console.table({ city: "Jaipur" });
-console.warn({ city: "Jaipur" });
